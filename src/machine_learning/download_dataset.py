c9f466355e34bfc2c36ee4b1ecefefd6bd89b297
Owen Senowitz <owen.senowitz@gmail.com>
2025-10-24 14:59:45 -0400

    fixed download dataset

--- src/machine_learning/download_dataset.py
+++ src/machine_learning/download_dataset.py
@@ -1,17 +1,22 @@
 import os
 from dotenv import load_dotenv
-from kaggle.api.kaggle_api_extended import KaggleApi
 
 # Load Kaggle credentials from .env
-load_dotenv()
+load_dotenv("../../.env")
 
 username = os.getenv("KAGGLE_USERNAME")
 key = os.getenv("KAGGLE_KEY")
 
+from kaggle.api.kaggle_api_extended import KaggleApi
+
 print("Using Kaggle account:", username)
 
+# Set Kaggle credentials as environment variables for KaggleApi
+os.environ["KAGGLE_USERNAME"] = username
+os.environ["KAGGLE_KEY"] = key
+
 # Directory where you want the dataset
-dataset_dir = "./data"
+dataset_dir = "../../data"
 
 # Check if dataset already exists
 if os.path.exists(dataset_dir) and len(os.listdir(dataset_dir)) > 0:
